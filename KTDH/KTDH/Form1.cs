2520d7bdf06036501c60bd16cdaa198b21bb4b8f
Khoa Tran <44254391+ankhoa18599@users.noreply.github.com>
2020-04-27 08:48:06 +0700

    HCN

--- KTDH/KTDH/Form1.cs
+++ KTDH/KTDH/Form1.cs
@@ -12,7 +12,8 @@ namespace KTDH
     public partial class Form1 : Form
     {
         public static int hinh = -1; // Xác định in hình nào để in thông số tọa độ hình đó ra 
-        Line AB;
+        Line AB,CD,BC,AD;
+        HinhChuNhat hcn;
         public Form1()
         {
             InitializeComponent();
@@ -39,23 +40,66 @@ namespace KTDH
         {
             if (Form1.hinh == 1)
             {
-                label5.Text = "Đường Thẳng";
-                label6.Text = "xA:";
-                label7.Text = "yA:";
-                label8.Text = ((AB.getFpoint().getX() - 200) / 5).ToString();
-                label9.Text = (-(AB.getFpoint().getY() - 200) / 5).ToString();
-                label10.Text = "xB:";
-                label11.Text = "yB:";
-                label12.Text = ((AB.getLpoint().getX() - 200) / 5).ToString();
-                label13.Text = (-(AB.getLpoint().getY() - 200) / 5).ToString();
-                label14.Text = "";
-                label15.Text = "";
-                label16.Text = "";
-                label17.Text = "";
-                label18.Text = "";
-                label19.Text = "";
-                label20.Text = "";
-                label21.Text = "";
+                if (Form1.hinh == -1)
+                {
+                    label5.Text = "";
+                    label6.Text = "";
+                    label7.Text = "";
+                    label8.Text = "";
+                    label9.Text = "";
+                    label10.Text = "";
+                    label11.Text = "";
+                    label12.Text = "";
+                    label13.Text = "";
+                    label14.Text = "";
+                    label15.Text = "";
+                    label16.Text = "";
+                    label17.Text = "";
+                    label18.Text = "";
+                    label19.Text = "";
+                    label20.Text = "";
+                    label21.Text = "";
+                }
+                else if (Form1.hinh == 1)
+                {
+                    label5.Text = "Đường Thẳng";
+                    label6.Text = "xA:";
+                    label7.Text = "yA:";
+                    label8.Text = ((AB.getFpoint().getX() - 200) / 5).ToString();
+                    label9.Text = (-(AB.getFpoint().getY() - 200) / 5).ToString();
+                    label10.Text = "xB:";
+                    label11.Text = "yB:";
+                    label12.Text = ((AB.getLpoint().getX() - 200) / 5).ToString();
+                    label13.Text = (-(AB.getLpoint().getY() - 200) / 5).ToString();
+                    label14.Text = "";
+                    label15.Text = "";
+                    label16.Text = "";
+                    label17.Text = "";
+                    label18.Text = "";
+                    label19.Text = "";
+                    label20.Text = "";
+                    label21.Text = "";
+                }
+                else if (Form1.hinh == 2)
+                {
+                    label5.Text = "Hình chữ nhật";
+                    label6.Text = "xA:";
+                    label7.Text = "yA:";
+                    label8.Text = ((hcn.AB.getFpoint().getX() - 200) / 5).ToString();
+                    label9.Text = (-(hcn.AB.getFpoint().getY() - 200) / 5).ToString();
+                    label10.Text = "xB:";
+                    label11.Text = "yB:";
+                    label12.Text = ((hcn.AB.getLpoint().getX() - 200) / 5).ToString();
+                    label13.Text = (-(hcn.AB.getLpoint().getY() - 200) / 5).ToString();
+                    label14.Text = "xC";
+                    label15.Text = "yC";
+                    label16.Text = ((hcn.BC.getLpoint().getX() - 200) / 5).ToString();
+                    label17.Text = (-(hcn.BC.getLpoint().getY() - 200) / 5).ToString();
+                    label18.Text = "xD";
+                    label19.Text = "yD";
+                    label20.Text = ((hcn.CD.getLpoint().getX() - 200) / 5).ToString();
+                    label21.Text = ((hcn.BC.getLpoint().getY() - 200) / 5).ToString();
+                }
             }
         }
         private void button1_Click(object sender, EventArgs e)
@@ -75,5 +119,18 @@ namespace KTDH
             labelChange(); // reset bang 
         }
 
+        private void JbntHInhChuNhat_Click(object sender, EventArgs e)
+        {
+            Form1.hinh = 2;
+            hcn = new HinhChuNhat();
+            hcn.ShowDialog();
+
+            hcn.AB.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.CD.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.BC.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.AD.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            labelChange();
+            //test 1:
+        }
     }
 }
---
+++ KTDH/KTDH/HinhChuNhat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KTDH
+{
+    public partial class HinhChuNhat : Form
+    {
+        public int xA, yA, a, b; //a dài, b rộng
+        public Line AB, BC, CD, AD;
+
+        private void jbntVeHCN_Click(object sender, EventArgs e)
+        {
+            xA = Int32.Parse(numericUpDown1.Value.ToString());
+            yA = Int32.Parse(numericUpDown2.Value.ToString());
+            a = Int32.Parse(numericUpDown3.Value.ToString());
+            b = Int32.Parse(numericUpDown4.Value.ToString());
+            int xa, xb, xc, xd, ya, yb, yc, yd;
+            xa = xA;
+            ya = yA;
+            xb = xa + a;
+            yb = ya;
+            xc = xa + a;
+            yc = ya + b;
+            xd = xa;
+            yd = ya + b;
+
+            Point A = Point.FakeToReal(xa, ya);
+            Point B = Point.FakeToReal(xb, yb);
+            Point C = Point.FakeToReal(xc, yc);
+            Point D = Point.FakeToReal(xd, yd);
+
+            AB = new Line(A.getX(), A.getY(), B.getX(), B.getY(), Color.Red);
+            BC = new Line(B.getX(), B.getY(), C.getX(), C.getY(), Color.Red);
+            CD = new Line(C.getX(), C.getY(), D.getX(), D.getY(), Color.Red);
+            AD = new Line(A.getX(), A.getY(), D.getX(), D.getY(), Color.Red);
+            this.Close();
+        }
+
+        private void HinhChuNhat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public HinhChuNhat()
+        {
+            InitializeComponent();
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
