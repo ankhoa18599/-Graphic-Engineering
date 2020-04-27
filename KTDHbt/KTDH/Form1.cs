15523d425dd88bf9653c246d1a514aca18777129
DuongTanPhat <43169143+DuongTanPhat@users.noreply.github.com>
2020-04-27 11:00:59 +0700

    Revert "Merge branch 'master' of https://github.com/ankhoa18599/-Graphic-Engineering"
    This reverts commit 001c5cbd724c1a932a71d3d7d2b6035f6b83970c, reversing
    changes made to a6b2e9246b9a8aa888d4d007b7b8a83d59147ac2.

--- KTDHbt/KTDH/Form1.cs
+++ KTDHbt/KTDH/Form1.cs
@@ -70,8 +70,28 @@ namespace KTDH
             yb = dt.yB;
             Point p1 = Point.FakeToReal(xa, ya); // gan toa do tren Oxy 
             Point p2 = Point.FakeToReal(xb, yb);
+<<<<<<< Updated upstream
             AB = new Line(p1.getX(), p1.getY(), p2.getX(), p2.getY(), Color.Red); //tao AB 
             AB.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics()); // ve dt AB bang DDA 
+=======
+            AB = new Line(p1.getX(), p1.getY(), p2.getX(), p2.getY(), Color.Red); //tao AB
+            if (luachonNet == 0) // ve dut khuc
+            {
+                AB.DDA_Line_ngat(this.JpnLuoiGiaoDien.CreateGraphics()); // ve dt AB bang DDA 
+            } else if (luachonNet == 1) // ve dut khuc
+            {
+                AB.DDA_Line_ngat_1cham(this.JpnLuoiGiaoDien.CreateGraphics()); // ve dt AB bang DDA 
+            }
+            else if (luachonNet == 2) // ve dut khuc
+            {
+                AB.DDA_Line_ngat_2cham(this.JpnLuoiGiaoDien.CreateGraphics()); // ve dt AB bang DDA 
+            }
+            else
+            {
+                AB.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics()); // ve dt AB bang DDA 
+                Test(AB);
+            }
+>>>>>>> Stashed changes
             labelChange(); // reset bang 
         }
 
@@ -79,5 +99,113 @@ namespace KTDH
         {
 
         }
+<<<<<<< Updated upstream
+=======
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            luachonNet = 0;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            luachonNet = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            luachonNet = 2;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            luachonNet = 3;
+        }
+
+        private void jbtnXoa_Click(object sender, EventArgs e)
+        {
+            Form1.hinh = -1;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            luachonNet = -1;
+            labelChange();
+            this.Refresh();
+        }
+
+        private void JbntHInhChuNhat_Click(object sender, EventArgs e)
+        {
+            Form1.hinh = 2;
+            hcn = new HinhChuNhat();
+            hcn.ShowDialog();
+
+            hcn.AB.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.CD.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.BC.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            hcn.AD.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            labelChange();
+            //test 1:
+        }
+
+        private void Test(Line AB)
+        {
+            //Khoảng cách DX,DY
+            double ABDx = Math.Abs(AB.getLpoint().getX() - AB.getFpoint().getX());
+            double ABDy = Math.Abs(AB.getLpoint().getY() - AB.getFpoint().getY());
+            //Độ dài AB
+            double DAB = Math.Sqrt(Math.Pow(ABDx, 2) + Math.Pow(ABDy, 2));
+
+            //chiều cao và cạnh đáy,góc tam giác mũi tên
+            double HArrTri = 40;  //DAB / 5;
+            double DayArrTri = 20;  //1.6;
+            double CanhBArrTri = Math.Sqrt(Math.Pow(HArrTri, 2) + Math.Pow(DayArrTri / 2, 2));
+            double G_Anpha = Math.Atan(DayArrTri / 2 / HArrTri) * 2 * 180 / Math.PI;
+            // Tính chất tam giác cân;
+            double G_Anpha1, G_Anpha2;
+            G_Anpha1 = G_Anpha2 = G_Anpha / 2;
+
+            //Góc B1 &B2
+            double G_B1 = Math.Atan(ABDy / ABDx) * 180 / Math.PI;
+            double G_B2 = Math.Atan(ABDx / ABDy) * 180 / Math.PI;
+
+            //Góc Beta
+            double G_Beta1, G_Beta2;
+            G_Beta1 = Math.Abs(G_B1 - G_Anpha1);
+            G_Beta2 = Math.Abs(G_B2 - G_Anpha2);
+
+            //Khoảng cách dy dx
+            double Dx1, Dy1, Dx2, Dy2;
+            Dy1 = Math.Sin(G_Beta1 * Math.PI / 180) * CanhBArrTri;
+            Dx1 = Math.Sqrt(Math.Pow(CanhBArrTri, 2) - Math.Pow(Dy1, 2));
+            Dx2 = Math.Sin(G_Beta2 * Math.PI / 180) * CanhBArrTri;
+            Dy2 = Math.Sqrt(Math.Pow(CanhBArrTri, 2) - Math.Pow(Dx2, 2));
+
+
+            Point MT1 = new Point();
+            Point MT2 = new Point();
+            if (AB.getLpoint().getX() > AB.getFpoint().getX())
+            {
+                Dx1 = -Dx1;
+                Dy1 = -Dy1;
+                Dx2 = -Dx2;
+                Dy2 = -Dy2;
+            }
+            double x1, x2, y1, y2;
+            x1 = Math.Round(AB.getLpoint().getX() + Dx1);
+            y1 = Math.Round(AB.getLpoint().getY() + Dy1);
+            x2 = Math.Round(AB.getLpoint().getX() + Dx2);
+            y2 = Math.Round(AB.getLpoint().getY() + Dy2);
+
+            MT1.setPoint((int)x1, (int)y1);
+            MT2.setPoint((int)x2, (int)y2);
+
+            Line Arrow1 = new Line(AB.getLpoint().getX(), AB.getLpoint().getY(), MT1.getX(), MT1.getY(), Color.HotPink);
+            Line Arrow2 = new Line(AB.getLpoint().getX(), AB.getLpoint().getY(), MT2.getX(), MT2.getY(), Color.HotPink);
+
+            Arrow1.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+            Arrow2.DDA_Line(this.JpnLuoiGiaoDien.CreateGraphics());
+        }
+>>>>>>> Stashed changes
     }
 }
