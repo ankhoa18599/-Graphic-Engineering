6ba7dbf5f0cfea2207043a262dab9c8d64c37e3f
Khoa Tran <44254391+ankhoa18599@users.noreply.github.com>
2020-05-22 16:03:37 +0700

    Revert3D

---
+++ KTDH/KTDH/KTDH/HinhThoi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace KTDH
+{
+    class HinhThoi
+    {
+        private Point Tam;
+        private Line CheoA;
+        private Line CheoB;
+        public void setTam(int x, int y)
+        {
+            this.Tam.setX(x);
+            this.Tam.setY(y);
+        }
+        public void setTam(Point tam)
+        {
+            this.Tam = tam;
+        }
+        public void setCheoA(Line a)
+        {
+            this.CheoA = a;
+        }
+        public void setCheoB(Line b)
+        {
+            this.CheoB = b;
+        }
+        public Point getTam()
+        {
+            return this.Tam;
+        }
+        public Line getCheoA()
+        {
+            return this.CheoA;
+        }
+        public Line getCheoB()
+        {
+            return this.CheoB;
+        }
+        public void setCheo(Line a, Line b)
+        {
+            setCheoA(a);
+            setCheoB(b);
+        }
+        public void Draw(Graphics g)
+        {
+            Line a1b1, b1a2, a2b2, b2a1;
+            Point AF, AL, BF, BL;
+            AF = new Point(CheoA.getFpoint().getX(), CheoA.getFpoint().getY());
+            AL = new Point(CheoA.getLpoint().getX(), CheoA.getLpoint().getY());
+            BF = new Point(CheoB.getFpoint().getX(), CheoB.getFpoint().getY());
+            BL = new Point(CheoB.getLpoint().getX(), CheoB.getLpoint().getY());
+
+            AF.setPoint(Point.round(AF.getX()), Point.round(AF.getY()));
+            AL.setPoint(Point.round(AL.getX()), Point.round(AL.getY()));
+            BF.setPoint(Point.round(BF.getX()), Point.round(BF.getY()));
+            BL.setPoint(Point.round(BL.getX()), Point.round(BL.getY()));
+
+            a1b1 = new Line(AF.getX(),AF.getY(),BF.getX(),BF.getY(),System.Drawing.Color.Black);
+            b1a2 = new Line(BF.getX(), BF.getY(), AL.getX(), AL.getY(), System.Drawing.Color.Black);
+            a2b2 = new Line(AL.getX(), AL.getY(), BL.getX(), BL.getY(), System.Drawing.Color.Black);
+            b2a1 = new Line(BL.getX(), BL.getY(), AF.getX(), AF.getY(), System.Drawing.Color.Black);
+            
+            a1b1.DDA_Line(g);
+            b1a2.DDA_Line(g);
+            a2b2.DDA_Line(g);
+            b2a1.DDA_Line(g);
+        }
+    }
+}
---
+++ KTDH/KTDHbt/KTDH/Point.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace KTDH
+{
+    public class Point
+    {
+        private int X;
+        private int Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public Point(Point a)
+        {
+            this.X = a.X;
+            this.Y = a.Y;
+        }
+        public Point() { }
+        public int getX() { return this.X; }
+        public int getY() { return this.Y; }
+        public void setX(int value) { this.X = value; }
+        public void setY(int value) { this.Y = value; }
+        public void setPoint(int x, int y)
+        {
+            setX(x); setY(y);
+        }
+        public static void putpixel(int x, int y, Graphics grfx)
+        {
+            if (x < 0 || x > 400 || y < 0 || y > 400) return;
+            Pen p = new Pen(Color.Black);
+            SolidBrush b = new SolidBrush(Color.Black);// Brush dùng để tô vùng bên trong của một hình 
+            // Lớp Brush là một lớp Abstract • Các lớp kế thừ từ lớp Brush 
+            //Một Solid Brush là một brush dùng để tô một vùng với một màu đơn.
+            grfx.DrawRectangle(p, x, y, 2, 2);//        |
+            ////////////////////////////////////---điểm click ---
+            ////////////////////////////////////        | tô ở đây
+            grfx.FillRectangle(b, x, y, 2, 2);
+
+            grfx.DrawRectangle(p, x - 2, y - 2, 2, 2); //     tô thêm ở đây   |
+            ////////////////////////////////////                       ---điểm click ---
+            ////////////////////////////////////                              | tô ở đây
+            grfx.FillRectangle(b, x - 2, y - 2, 2, 2);
+
+            grfx.DrawRectangle(p, x, y - 2, 2, 2);//     tô thêm ở đây   | tô ở đây
+            ////////////////////////////////////                   ---điểm click ---
+            ////////////////////////////////////                         | tô ở đây
+            grfx.FillRectangle(b, x, y - 2, 2, 2);
+
+            grfx.DrawRectangle(p, x - 2, y, 2, 2);//     tô thêm ở đây   | tô ở đây
+            ////////////////////////////////////                   ---điểm click ---
+            ////////////////////////////////////         tô ở đây        | tô ở đây
+            grfx.FillRectangle(b, x - 2, y, 2, 2);
+            // và ta đã có 1 điểm pixel "fake" cấu thành từ 4 pixel nhỏ.
+        }
+        public static void putpixel_1real(int x, int y, Graphics grfx)
+        {
+            if (x < 0 || x > 400 || y < 0 || y > 400) return;
+            Pen p = new Pen(Color.Black);
+            SolidBrush b = new SolidBrush(Color.Black);// Brush dùng để tô vùng bên trong của một hình 
+            // Lớp Brush là một lớp Abstract • Các lớp kế thừ từ lớp Brush 
+            //Một Solid Brush là một brush dùng để tô một vùng với một màu đơn.
+            grfx.DrawRectangle(p, x, y, 2, 2);//        |
+            ////////////////////////////////////---điểm click ---
+            ////////////////////////////////////        | tô ở đây
+            grfx.FillRectangle(b, x, y, 2, 2);
+        }
+        public static int round(double inP)
+        {
+            int outP;
+            double sodu = inP % 5;
+            if (sodu != 0)
+            {
+                if (sodu >= 3) outP = (int)(inP + 5 - sodu); // làm tròn lên nếu gần phía trên hơn. 278 % 5 = 3 => out 278+5-3 =280
+                else outP = (int)(inP - sodu); // làm tròn xuống 272 % 5 = 2 => out 272 -2  =270
+            }
+            else outP = (int)inP; // 275 vẫn là 275
+            if (outP > 400) outP = 400; //nếu tọa độ lớn hơn 400 thì trả về 400
+            return outP;
+        }
+        public static Point RealToFake(int x, int y)//lon ra nho
+        {
+            return new Point(x / 5 - 40, 40 - y / 5);//voi x va y deu chia het cho 5
+        }
+        public static Point FakeToReal(int x, int y)//nho ra lon
+        {
+
+            return new Point((x + 40) * 5, (40 - y) * 5);
+        }
+    }
+}
